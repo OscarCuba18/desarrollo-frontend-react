4563a3711a397f3c3dbfa2f78ea4a27dc6f8daa3
OscarCuba18 <85810025+OscarCuba18@users.noreply.github.com>
2024-08-14 21:28:26 -0400

    Adding exercise props of Lesoninfo components into Default

--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,6 +1,7 @@
 import Logo from "./_components/Logo";
 import Description from "./_components/Description";
 import OpenLink from "../components/OpenLink";
+import Lesoninfo from "./_components/Lesoninfo"
 //import Github from "./_components/Github";
 const Default = () => {
     return (
@@ -16,7 +17,14 @@ const Default = () => {
                     title="Mi repositorio de React"
                     url="https://github.com/OscarCuba18/desarrollo-frontend-react"
                 />
-
+                <Lesoninfo
+                    number="1"
+                    title="Introducción a react y estructura de proyecto"
+                />
+                <Lesoninfo
+                    number="2"
+                    title="Context API para la gestión del estado global en aplicaciones react"
+                />
             </header>
         </>
     );
---
+++ src/screens/_components/Lesoninfo.jsx
@@ -0,0 +1,9 @@
+const Lesoninfo = ({number, title}) => {
+    return (
+        <>
+            <p><strong>{number}</strong>.- {title} </p>
+        </>
+    );
+};
+
+export default Lesoninfo;
