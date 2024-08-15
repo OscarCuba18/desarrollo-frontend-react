359f55f7acbcec8634eafe5ada1b79b397e57f78
OscarCuba18 <85810025+OscarCuba18@users.noreply.github.com>
2024-08-14 21:01:20 -0400

    Adding components files

---
+++ src/components/Github.jsx
@@ -0,0 +1,7 @@
+const Github = () => {
+    return (
+        <a href="https://github.com/OscarCuba18/desarrollo-frontend-react" target="_blank" rel="noopener noreferrer">Enlace a Github</a>
+    )
+}
+
+export default Github;
